43fb06931e114cb6566e74920bef8df4e1c6de3c
Devi Hainun Pasya <61291198+notdevi@users.noreply.github.com>
2021-11-19 22:43:14 +0800

    can decrypt but wrong

--- Tugas 2/test_tgs2.py
+++ Tugas 2/test_tgs2.py
@@ -31,30 +31,52 @@ def cbc_encrypt(k, plaintext) :
 def cbc_imp(k, plaintext) :
     encrypted = cbc_encrypt(k, plaintext)
     #decrypted = cbc_decrypt(k, iv, encrypted)
+    print(encrypted)
+
     return encrypted
 
 def enkrip_message(k, m) :
-    h = cbc_imp(k, m)
-
-    ciphertext1 = m + "||" + h
     ciphertext2 = ""
     index = 0
 
-    for i in ciphertext1 :
-        i = ord(i)
-        i = encrypt(k, i)     # encrypt i with key
-        i = i << 1            # geser 1 bit ke kiri (bitwise op)
+    for i in m :
+        i = encrypt(k, ord(i))
+        #i = i << 1
 
         ciphertext2 = ciphertext2 + chr(i)
         index = index + 1
 
     #enkrip = encrypt(k, ciphertext1)
+    return ciphertext2
 
-    print(ciphertext1, " - ", ciphertext2)
+def dekrip_message(k, c) :
+    plaintext2 = ""
+    index = 0
+
+    for i in c :
+        #i = ord(i) >> 1
+        i = decrypt(k, ord(i))
+
+        plaintext2 = plaintext2 + chr(i)
+        index = index + 1
+
+    #enkrip = encrypt(k, ciphertext1)
+
+    return plaintext2
+
+def iec_imp(k, m) :
+    h = cbc_imp(k, m)
+    ciphertext1 = m + "||" + h
+
+    cipher = enkrip_message(k, ciphertext1)
+    plain = dekrip_message(k, cipher)
+    print(ciphertext1, "-", cipher, "-", plain)
 
 if __name__ == "__main__" :
     k  = 0b1110101111
     m = "DEVIHAINUN"
     # k  = 0b1110101111
     iv = 0b0000000000
-    enkrip_message(k, m)
+    #iec_imp(k, m)
+    cbc_imp(k, m)
+
