ebaac7bac3d5db26bbc30660827c72dba8918e3b
Devi Hainun Pasya <61291198+notdevi@users.noreply.github.com>
2021-11-21 15:04:27 +0800

    iec done insya allah

--- Tugas 2/test_tgs2.py
+++ Tugas 2/test_tgs2.py
@@ -1,82 +1,74 @@
 import sdes
 from sdes import *
 
-def cbc_encrypt(k, plaintext) :
-    #k  = 0b1110101111
+def MAC(k, msg) :
     index = 0; flag = 0;
     current_text = ""
 
-    #iv_bit = ord(iv)
-    #k_bit = ord(k)
-
-    for i in plaintext :
+    for i in msg :
         if(flag == 0) :
             i = ord(i)
-            i = encrypt(k, i)     # encrypt i with key
-            i = i << 1            # geser 1 bit ke kiri (bitwise op)
+            i = encrypt(k, i)          
 
             flag = 1
             current_text = current_text + chr(i)
         
         else :
-            i = ord(i) ^ ord(plaintext[index-1])
+            i = ord(i) ^ ord(current_text[index-1])
             i = encrypt(k, i)
-            i = i << 1
 
             current_text = current_text + chr(i)
         index = index + 1
-
+    
     return current_text
 
-def cbc_imp(k, plaintext) :
-    encrypted = cbc_encrypt(k, plaintext)
-    #decrypted = cbc_decrypt(k, iv, encrypted)
-    print(encrypted)
+def MAC_imp(k, msg) :
+    encrypted = MAC(k, msg)
 
     return encrypted
 
 def enkrip_message(k, m) :
     ciphertext2 = ""
-    index = 0
 
     for i in m :
         i = encrypt(k, ord(i))
-        #i = i << 1
-
         ciphertext2 = ciphertext2 + chr(i)
-        index = index + 1
 
     #enkrip = encrypt(k, ciphertext1)
     return ciphertext2
 
 def dekrip_message(k, c) :
     plaintext2 = ""
-    index = 0
+    check = False
 
     for i in c :
-        #i = ord(i) >> 1
         i = decrypt(k, ord(i))
-
         plaintext2 = plaintext2 + chr(i)
-        index = index + 1
+
+    before = plaintext2.split("||")[0]
+    after = plaintext2.split("||")[1]
+
+    h = MAC_imp(k, m)
+    if(h == after) :
+        check = True
 
     #enkrip = encrypt(k, ciphertext1)
-
-    return plaintext2
+    return before, check
 
 def iec_imp(k, m) :
-    h = cbc_imp(k, m)
-    ciphertext1 = m + "||" + h
+    h = MAC_imp(k, m)
+    cipher_append_in = m + "||" + h
 
-    cipher = enkrip_message(k, ciphertext1)
-    plain = dekrip_message(k, cipher)
-    print(ciphertext1, "-", cipher, "-", plain)
+    cipher_internal = enkrip_message(k, cipher_append_in)
+    plain_internal, hasil_check_internal = dekrip_message(k, cipher_internal)
+
+    print("MAC Hash Only : ", h)
+    print("Appended Hash : ", cipher_append_in)    
+    print("Decrypt & Check Result : ", plain_internal, "-", hasil_check_internal)
 
 if __name__ == "__main__" :
     k  = 0b1110101111
     m = "DEVIHAINUN"
-    # k  = 0b1110101111
-    iv = 0b0000000000
-    #iec_imp(k, m)
-    cbc_imp(k, m)
 
+    iec_imp(k, m)
+    #MAC_imp(k, m)
