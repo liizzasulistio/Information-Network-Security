34d14403f726af582ad469cd738720e6c9503ab1
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-11-22 11:11:57 +0700

    external done, I guess

--- Tugas 2/test_tgs2.py
+++ Tugas 2/test_tgs2.py
@@ -64,11 +64,54 @@ def iec_imp(k, m) :
 
     print("MAC Hash Only : ", h)
     print("Appended Hash : ", cipher_append_in)    
+    # print("Encrypt : ", cipher_internal)    # buat check aja
     print("Decrypt & Check Result : ", plain_internal, "-", hasil_check_internal)
+
+
+def encrypt_message_ext(k, m):
+    ciphertext_ext = ""
+
+    # Melakukan enkripsi pada message yang akan dikirimkan
+    for i in m:
+        i = encrypt(k, ord(i)) # enkripsi dengan library sdes
+        ciphertext_ext = ciphertext_ext + chr(i)
+
+    # Menambahkan MAC hash pada message yang akan dikirimkan
+    cipher_append_ext = ciphertext_ext + "||" + MAC_imp(k, m)
+    return cipher_append_ext
+
+def decrypt_message_ext(k, m, check):
+    a = "" # plaintext
+    checkValid = False # check validasi
+    FM = "" # MAC hash yang dibawa oleh message
+    
+    # Melakukan pemisahan pada message yg telah dikirimkan
+    message = m.split("||")[0]
+    FM = m.split("||")[1]
+
+    # cek apakah MAC asli sama dengan yang dibawa oleh message
+    if(check == FM):
+        checkValid = True
+
+    # Dekripsi message yang dikirimkan
+    for i in message:
+        i = decrypt(k, ord(i)) # dekripsi dengan library sdes
+        a += chr(i)
+
+    return a, checkValid
+
+def cipher_ext(k, m):
+    check = MAC_imp(k, m)
+
+    encrypt_external = encrypt_message_ext(k, m)
+    decrypt_external, checkFM = decrypt_message_ext(k, encrypt_external, check)
+    print("Ciphertext External : ", encrypt_external)
+    print("Decrypt External : ", decrypt_external, '-', checkFM)
 
 if __name__ == "__main__" :
     k  = 0b1110101111
     m = "DEVIHAINUN"
 
     iec_imp(k, m)
+    cipher_ext(k, m)
     #MAC_imp(k, m)
