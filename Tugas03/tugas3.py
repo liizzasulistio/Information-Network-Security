73248b27f14c4afc639b7f1e656e4c981bf92646
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-11-29 13:17:58 +0700

    can encrypt, still error in decrypt

--- Tugas03/tugas3.py
+++ Tugas03/tugas3.py
@@ -15,17 +15,45 @@ def hash_message(message):
     # print(hashed_m)
     return hashed_m
 
-def encrypt_msg(message, private_key):
-    msg_hashed = hash_message(message)
+def encrypt_msg(message, hashed, private_key):
+    # msg_hashed = hash_message(message)
     encrypted_message = ""
 
     # for i in msg_hashed:
     #     i = encrypt(ord(i), private_key)
     #     encrypted_message += str(i)
-    encrypted_message = encrypt(msg_hashed, private_key)
-    print(encrypted_message)
+    encrypted_message = encrypt(hashed, private_key)
+    append_msg = message + "||" + str(encrypted_message)
+    # append_msg = message.append(encrypted_message)
 
+    # print(append_msg)
+    return append_msg
 
+def decrypt_msg(encrypted_message, hashed, public_key):
+    message = ""
+    checkValid = False
+
+    # print(delivered_message)
+    message = encrypted_message.split("||")[0]
+    hashToBeCheck = encrypted_message.split("||")[1]
+    # print(message)
+    # print(hashToBeCheck)
+
+    decrypt_hash = decrypt(hashToBeCheck, public_key)
+    # print(hashed)
+
+    # if(hashed == decrypt_hash):
+    #     checkValid = True
+
+    # return message, checkValid
+    
+def rsa_implementation(message, private_key, public_key):
+    hashed = hash_message(message)
+    encrypted_message = encrypt_msg(message, hashed, private_key)
+    # print("Delivered: " + encrypted_message)
+    decrypt_msg(encrypted_message, hashed, public_key)
+    # decrypted_message, checkValid = decrypt_msg(encrypted_message, hashed, public_key)
+    # print("Received: " + decrypted_message, checkValid)
 
 
 
@@ -37,4 +65,10 @@ if __name__ == '__main__':
     print("Private Key: ",  private_key)
     print("Public Key: ", public_key)
     # hash_message(message)
-    encrypt_msg(message, private_key)
+    rsa_implementation(message, private_key, public_key)
+    # delivered_msg = encrypt_msg(message, private_key)
+    # received_msg, check = decrypt_msg(delivered_msg, public_key)
+    # print("Delivered Message: " + delivered_msg)
+    # print("Received Message: " + received_msg)
+    # print("Check: " + str(check))
+
